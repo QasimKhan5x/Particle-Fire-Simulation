4a250fd074f40775ea58276337766481f7648d7c
QasimKhan5x <straightouttadetroit.qk@gmail.com>
2020-10-31 16:39:13 +0500

    animate colors

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
+#include <math.h>
 #include "Screen.cpp"
+
 using namespace std;
 using namespace pfs;
 
@@ -15,14 +17,17 @@ int main(int argv, char** args) {
 		// Update particles
 		
 		// Draw particles
+		// number of ms since program started
+		int elapsed = SDL_GetTicks();
+		unsigned char red = (1 + sin(elapsed * 0.0001)) * 128;
+		unsigned char green = (1 + sin(elapsed * 0.0002)) * 128;
+		unsigned char blue = (1 + sin(elapsed * 0.0003)) * 128;
 		for(int y=0; y < Screen::SCREEN_HEIGHT; y++) {
 			for(int x=0; x < Screen::SCREEN_WIDTH; x++) {
-				screen.setPixel(x, y, 128, 0, 255);
+				screen.setPixel(x, y, red, green, blue);
 			}
 		}
 		
-		screen.setPixel(400, 300, 255, 255, 255);
-
 		// Draw the screen
 		screen.update();
 
