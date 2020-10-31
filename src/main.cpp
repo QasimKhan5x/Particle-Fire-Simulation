8d04f3fa8617f33429bddb691c81c4f581794be6
QasimKhan5x <straightouttadetroit.qk@gmail.com>
2020-10-31 16:39:51 +0500

    animate colors

--- src/main.cpp
+++ src/main.cpp
@@ -19,6 +19,8 @@ int main(int argv, char** args) {
 		// Draw particles
 		// number of ms since program started
 		int elapsed = SDL_GetTicks();
+		
+		// color changing algorithm
 		unsigned char red = (1 + sin(elapsed * 0.0001)) * 128;
 		unsigned char green = (1 + sin(elapsed * 0.0002)) * 128;
 		unsigned char blue = (1 + sin(elapsed * 0.0003)) * 128;
